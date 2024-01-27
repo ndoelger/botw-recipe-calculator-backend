61d43ec96045e60b6ad9e7f52380c16fc740e66a
ndoelger <doelgern@gmail.com>
2024-01-26 23:31:49 -0500

    updated from notion app

--- routes/materials.js
+++ routes/materials.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-const ingredients = require("../ingredients.json");
+const materials = require("../materials.json");
 
 router.get('/', async function(req, res) {
     try {
       // get all items
-      res.json(ingredients);
+      res.json(materials);
     } catch (error) {
       //send error
       res.status(400).json(error);
--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const express = require('express');
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+
+const PORT = 3001;
+
+const express = require("express");
 const app = express();
-const port = 3000;
 
-const ingredientsRouter = require('./routes/ingredients');
+const cors = require("cors")
+const morgan = require("morgan")
+
+const ingredientsRouter = require("./routes/materials")
+
+///////////////////////////////
+// MIDDLEWARE
+////////////////////////////////
+app.use(express.json()); 
+app.use(cors()); 
+app.use(morgan("dev"));
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use("/ingredients", ingredientsRouter);
 
 
-// app.get('/', (req, res) => res.send('Hello World!'));
-
-app.use("/", ingredientsRouter);
-
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
